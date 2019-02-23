61d9c4b04e9b52d20f0b0e14ed9f64626321e2e4
wcolts2000 <wcolts2000@gmail.com>
2019-02-23 11:58:22 -0800

    added signin component with firebase auth and redirect to dashboard if user is admin authenticated

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/home";
+import SignIn from "./components/signin";
 import Layout from "./HOC/Layout";
 
 const Routes = props => (
   <Layout>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/signin" component={SignIn} />
     </Switch>
   </Layout>
 );
--- src/components/home/promotion/Enroll.js
+++ src/components/home/promotion/Enroll.js
@@ -89,7 +89,6 @@ export default class Enroll extends Component {
             this.resetFormSuccess(false);
           }
         });
-      //this.resetFormSuccess();
     } else {
       this.setState({
         formError: true
